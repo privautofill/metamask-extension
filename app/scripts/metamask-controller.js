53830b2c7affcb3019f48a304d8fde06a125c4c3
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-05-22 17:34:37 -0400

    fixing the actions

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -3065,7 +3065,6 @@ export default class MetamaskController extends EventEmitter {
       userStorageController,
       metamaskNotificationsController,
       pushPlatformNotificationsController,
-      metaMetricsDataDeletionController,
     } = this;
 
     return {
@@ -3852,12 +3851,12 @@ export default class MetamaskController extends EventEmitter {
       setName: this.nameController.setName.bind(this.nameController),
       // metrics data deleteion
       createMetaMetricsDataDeletionTask:
-        metaMetricsDataDeletionController.createMetaMetricsDataDeletionTask.bind(
-          metaMetricsDataDeletionController,
+        this.metaMetricsDataDeletionController.createMetaMetricsDataDeletionTask.bind(
+          this.metaMetricsDataDeletionController,
         ),
-      checkDataDeletionTaskStatus:
-        metaMetricsDataDeletionController.checkDataDeletionTaskStatus.bind(
-          metaMetricsDataDeletionController,
+      updateDataDeletionTaskStatus:
+        this.metaMetricsDataDeletionController.updateDataDeletionTaskStatus.bind(
+          this.metaMetricsDataDeletionController,
         ),
     };
   }
